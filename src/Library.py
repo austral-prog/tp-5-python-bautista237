2dcef8ead7f886696dbf2610bd13a3efe6f26252
bautista237 <162723056+bautista237@users.noreply.github.com>
2024-09-12 18:58:53 -0300

    Update Library.py

--- src/Library.py
+++ src/Library.py
@@ -1,32 +1,28 @@
-from typing import List, Optional
-from Book import Book
-from User import User
+from src.Book import Book
+from src.User import User
 
 class Library:
     def __init__(self) -> None:
-        self.books: List[Book] = []
-        self.users: List[User] = []
+        self.books: list[Book] = []
+        self.users: list[User] = []
 
     def add_book(self, book: Book) -> None:
         self.books.append(book)
 
-    def remove_book(self, book_id: int) -> None:
-        self.books = [book for book in self.books if book.get_book_id() != book_id]
+    def remove_book(self, book: Book) -> None:
+        self.books.remove(book)
 
     def add_user(self, user: User) -> None:
         self.users.append(user)
 
-    def remove_user(self, user_id: int) -> None:
-        self.users = [user for user in self.users if user.get_user_id() != user_id]
+    def remove_user(self, user: User) -> None:
+        self.users.remove(user)
 
-    def find_book_by_id(self, book_id: int) -> Optional[Book]:
+    def find_book(self, title: str) -> Book | None:
         for book in self.books:
-            if book.get_book_id() == book_id:
+            if book.title == title:
                 return book
         return None
 
-    def find_user_by_id(self, user_id: int) -> Optional[User]:
-        for user in self.users:
-            if user.get_user_id() == user_id:
-                return user
-        return None
+    def is_user_registered(self, user: User) -> bool:
+        return user in self.users

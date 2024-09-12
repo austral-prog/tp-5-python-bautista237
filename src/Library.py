9b5f836cedc4d97051bf99895d0b3f652479dec5
bautista237 <162723056+bautista237@users.noreply.github.com>
2024-09-12 18:55:04 -0300

    Update Library.py

--- src/Library.py
+++ src/Library.py
@@ -1,7 +1,11 @@
+from typing import List, Optional
+from Book import Book
+from User import User
+
 class Library:
     def __init__(self) -> None:
-        self.books = []
-        self.users = []
+        self.books: List[Book] = []
+        self.users: List[User] = []
 
     def add_book(self, book: Book) -> None:
         self.books.append(book)
@@ -15,13 +19,13 @@ class Library:
     def remove_user(self, user_id: int) -> None:
         self.users = [user for user in self.users if user.get_user_id() != user_id]
 
-    def find_book_by_id(self, book_id: int) -> Book | None:
+    def find_book_by_id(self, book_id: int) -> Optional[Book]:
         for book in self.books:
             if book.get_book_id() == book_id:
                 return book
         return None
 
-    def find_user_by_id(self, user_id: int) -> User | None:
+    def find_user_by_id(self, user_id: int) -> Optional[User]:
         for user in self.users:
             if user.get_user_id() == user_id:
                 return user
